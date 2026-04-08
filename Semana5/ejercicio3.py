11491f41190849129076abec940de2fba412a76e
angelica-radas <angelica.radas@cloud.uautonoma.cl>
2026-04-08 19:05:09 -0400

    calcular factorial con iteración y recursividad

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,8 +6,17 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
-    pass
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
 
 
 def factorial_recursivo(n):
-    pass
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial_recursivo(n- 1)
+
+print(factorial_ciclo(5))
+print(factorial_recursivo(5))
