2a6c2074ae91c5c64e4ade4c8e9d344bb87e0afc
angelica-radas <angelica.radas@cloud.uautonoma.cl>
2026-04-08 19:18:05 -0400

    métodos para calcular potencia con iteración y recursividad

--- Semana5/ejercicio5.py
+++ Semana5/ejercicio5.py
@@ -4,8 +4,17 @@ Calcular la potencia de una base elevada a un exponente entero positivo.
 """
 
 def potencia_ciclo(base, exponente):
-    pass
+    resultado = 1
+    for i in range(exponente):
+        resultado *= base
+    return resultado
 
 
 def potencia_recursiva(base, exponente):
-    pass
+    if exponente == 0:
+        return 1
+    else:
+        return base * potencia_recursiva(base, exponente - 1)
+
+print(potencia_ciclo(2, 8))
+print(potencia_recursiva(2, 8))
