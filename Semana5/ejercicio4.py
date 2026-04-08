7e2e74503f1d4c0307d0f58c152ded20498f3838
angelica-radas <angelica.radas@cloud.uautonoma.cl>
2026-04-08 19:14:42 -0400

    métodos contar_pares con iteración y recursividad

--- Semana5/ejercicio4.py
+++ Semana5/ejercicio4.py
@@ -4,8 +4,19 @@ Dado un número entero positivo N, contar cuántos números pares existen entre 
 """
 
 def contar_pares_ciclo(n):
-    pass
+    contador = 0
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            contador +=1
+    return contador
 
 
 def contar_pares_recursivo(n):
-    pass
+    if n <= 1:
+        return 0
+    else: 
+        es_par = 1 if n % 2 == 0 else 0
+        return es_par + contar_pares_recursivo(n - 1)    
+
+print(contar_pares_ciclo(9))
+print(contar_pares_recursivo(9))
